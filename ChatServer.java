235004d066464d81367cd9ba86c4e8de07dd8d87
Alexander <knipex@bk.ru>
2020-05-25 17:22:57 +0300

    lesson-4. Домашнее задание

---
+++ ChatServer.java
@@ -0,0 +1,11 @@
+package ru.gb.java2_stream200512_lesson_4;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
